571fdfcf712d0f557820351e34a45d4dbca8eaab
elephant97 <elephant0908@gmail.com>
2022-12-13 22:19:41 +0900

    #8 - 데이터베이스 접근 로직 구현
    데이터베이스 접근 로직 구현 03번 수강 내용 중간 커밋

--- src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -4,4 +4,5 @@ import com.fastcampus.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
 }
