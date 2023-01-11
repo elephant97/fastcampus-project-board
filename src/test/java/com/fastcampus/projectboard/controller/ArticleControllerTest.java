83dba2450ce950e0852090de81c886f84ba5ca54
elephant97 <elephant0908@gmail.com>
2023-01-12 01:26:36 +0900

    #15 - 사용하지 않는 import제거
    이전 수정이 이루어진 부분을 통해 사용하지 않게 된 import를 제거하는 작업 진행

--- src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -1,12 +1,10 @@
 package com.fastcampus.projectboard.controller;
 
-import com.fastcampus.projectboard.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,7 +12,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
-@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class) //지정 컨트롤러만 테스트 하도록
 class ArticleControllerTest {
     private final MockMvc mvc;
